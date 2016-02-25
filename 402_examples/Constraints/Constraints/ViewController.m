6899aead5058d5056481ba28102e6a54ba61e8ab
luizperes <luiz_peres10@hotmail.com>
2016-02-24 17:15:20 -0800

    Constraints exercise

---
+++ 402_examples/Constraints/Constraints/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Constraints
+//
+//  Created by CICCC1 on 2016-02-24.
+//  Copyright © 2016 Ideia do Luiz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
