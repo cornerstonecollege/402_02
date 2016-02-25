070078927e8afbd46f9983cf0436ce79b1b012dc
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2016-02-24 19:28:12 -0800

    make a balloon app

---
+++ Hiroshi/MusicGame/MusicGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicGame
+//
+//  Created by Hiroshi on 2/24/16.
+//  Copyright © 2016 Hiroshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
