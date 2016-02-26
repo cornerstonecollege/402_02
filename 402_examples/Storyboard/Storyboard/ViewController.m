aaa9fec8d98d69167e8c04fcfd20d1195b7a86ae
luizperes <luiz_peres10@hotmail.com>
2016-02-26 14:52:46 -0800

    Example with Storyboards

---
+++ 402_examples/Storyboard/Storyboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Storyboard
+//
+//  Created by CICCC1 on 2016-02-26.
+//  Copyright © 2016 Ideia do Luiz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
